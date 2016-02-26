a949f49094de3493b8fd74a0f27b11f5fc2500b8
leemuar <leemuar@gmail.com>
2016-02-26 23:08:12 +0300

    finished drill 19 of chapter 5
    fixed:
    - there was no type declared for vector v
    - wrong condition of for-loop caused out of range error

--- chapters/05/drill/19.cpp
+++ chapters/05/drill/19.cpp
@@ -8,7 +8,7 @@ remove all errors in program
 int main(){
 	
 try {
-	vector v(5); for (int i = 0; i<=v.size(); ++i); cout << "Success!\n";
+	vector<int> v(5); for (int i = 0; i<v.size(); ++i); cout << "Success!\n";
 	keep_window_open();
 	return 0;
 } catch(exception& e) {
