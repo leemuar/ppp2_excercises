46ca798b46296ba0da01e8dff3e0729452e22cde
leemuar <leemuar@gmail.com>
2016-02-27 14:00:16 +0300

    finished excercise 3 of chapter 5
    Summary:
    - rewrote program to use exceptions for errors
    - added user defined exceptions (Bad_Input, Input_Too_Low)
    - cerr stream is now used to report errors
    - added constant expression for lowest possible temperature in
    Celsius
    - added constant expressions for program exit codes
    - main() is now returning exit codes

--- chapters/05/excercises/03.cpp
+++ chapters/05/excercises/03.cpp
@@ -6,6 +6,10 @@ main program that will produce an error if a temperature is given below
 -273.15 C
 */
 
+// for bad input exception
+class Bad_Input{};
+// for invalid input exception
+class Input_Too_Low{};
 
 #include "std_lib_facilities.h"
 double ctok(double c)
@@ -16,15 +20,40 @@ double ctok(double c)
 
 int main()
 {
+	// lowest temperature possible in Celsius
+	constexpr double lowest_possible = -273.15;
+	// exit codes
+	constexpr int ok_exit_code = 0;
+	constexpr int bad_input_exit_code = 1;
+	constexpr int too_low_temp_exit_code = 2;
+	constexpr int unknown_error_exit_code = 3;
 	
 	double c=0; // declare input variable
 	cout << "Please enter a temperature in Celsius (as floating-point number): ";
 	cin >> c; // retrieve temperature to input variable
-	if (cin){
-		double k = ctok(c); // convert temperature
-		cout << c << " in Celsius is " << k << " in Kelvin\n";
-	} else {
-		cout << "Error! Wrong temperature provided! Please enter a floating point number\n";
+	try {
+	
+	// check for bad input
+	if (!cin){ throw Bad_Input{}; }
+	// check for lower boundary of temperature
+	if (c < lowest_possible) { throw Input_Too_Low{}; }
+	
+	double k = ctok(c); // convert temperature
+	cout << c << " in Celsius is " << k << " in Kelvin\n";
+	
+	// everyhing is just fine, terminate program nicely
+	return ok_exit_code;
+	
+	// deal with errors
+	} catch (Bad_Input){
+		cerr << "Error! Temperature entered is not valid";
+		return bad_input_exit_code;
+	} catch (Input_Too_Low) {
+		cerr << "Error! You have entered temperature lower than lowest possible value";
+		return too_low_temp_exit_code;
+	} catch (...){
+		cerr << "Unknown error happened";
+		return unknown_error_exit_code;
 	}
 
 }
