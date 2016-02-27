53a8c2cd21285577956c133b81ca5cc10b0e797c
leemuar <leemuar@gmail.com>
2016-02-27 15:04:28 +0300

    initial version for exercise 6 of chapter 5

---
+++ chapters/05/excercises/06.cpp
@@ -0,0 +1,15 @@
+/*
+Write a program that cpnverts from Celsius to Fahrenheit and from Fahr-
+enheit to Celsius (formula in 4.3.3). Use estimation (5.8) to see if your
+results are plausible
+*/
+
+#include "std_lib_facilities.h"
+
+
+int main()
+{
+
+	
+
+}
