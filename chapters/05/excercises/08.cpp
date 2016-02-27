98f9c6a148bb1246186752e15d7e39ced3e5e953
leemuar <leemuar@gmail.com>
2016-02-28 00:06:11 +0300

    Initial version for exercise 8 of chapter 5

---
+++ chapters/05/excercises/08.cpp
@@ -0,0 +1,16 @@
+/*
+Write a program that reads and stores a series of integers and then com-
+putes the sum of the first N integers. First ask for N, then read the values
+into a vector, then calculate the sum of the first N values.
+Handle all inputs. for example, make sure to give an error message if the
+user asks for a sum of more numbers than there are in the vector
+*/
+
+
+#include "std_lib_facilities.h"
+
+
+int main()
+{
+
+}
