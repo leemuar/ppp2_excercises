2a6dccf0e1668bbc2957553524eb9eb760e38821
leemuar <leemuar@gmail.com>
2016-03-01 22:17:49 +0300

    Started exercise 11 of chapter 5

---
+++ chapters/05/excercises/11.cpp
@@ -0,0 +1,14 @@
+/*
+Write a program that writes out the first so many values of the Fibonacci
+series, that is, the series that starts with 1 1 2 3 5 8 13 21 34. The next
+number of the series is the sum of the two previous ones. Fins the largest 
+Fibonacci number that fits in an int.
+*/
+
+
+#include "std_lib_facilities.h"
+
+int main()
+{
+	
+}
