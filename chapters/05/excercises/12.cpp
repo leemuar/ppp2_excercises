53c380ef60335883ba821669c7f3d0e28ae26bde
leemuar <leemuar@gmail.com>
2016-03-03 10:50:58 +0300

    started exercise 12 of chapter 5

---
+++ chapters/05/excercises/12.cpp
@@ -0,0 +1,19 @@
+/*
+Implement a littel guessing game called (for some obscure reason)
+"Bulls and Cows"/ THe program has a vector of four different integers in the
+range of 0 to 9 (e.g., 1234 but not 1122) and it is the user's task to discover
+those numbers by repeated guesses. Say the number to be guessed is 1234
+and user guesses 1359; the response should be "1 bull and 1 cow"
+because the user got one digit (1) right and in the right position (a bull)
+and one digit (3) right but in the wrong position (a cow). The guessing
+continues until the user gets four bulls, that is, has four digits correct
+and in the correct order
+*/
+
+
+#include "std_lib_facilities.h"
+
+int main()
+{
+	
+}
